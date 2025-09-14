bd1aff46797a9f25c2e8cfde0ff06f61bc3c481d
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-14 16:47:50 +0500

    Task7

---
+++ Tyuiu.NazarovAA.Sprint2.Task7.V2.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.NazarovAA.Sprint2.Task7.V2.Lib
+{
+    public class DataService : ISprint2Task7V2
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            return y <= x / 2 && x * x + y * y <= 1; 
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint2.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NazarovAA.Sprint2.Task7.V2.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint2.Task7.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCheckDotInShadedAreaValid()
+        {
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(0.5, -0.5);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
