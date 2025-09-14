f946172b52e4ac8cd133698acdaed8347b355c23
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-14 15:56:51 +0500

    Task5 fix1

--- Tyuiu.NazarovAA.Sprint2.Task5.V9.Lib/DataService.cs
+++ Tyuiu.NazarovAA.Sprint2.Task5.V9.Lib/DataService.cs
@@ -11,75 +11,75 @@ namespace Tyuiu.NazarovAA.Sprint2.Task5.V9.Lib
             {
                 case 1:
                     if (n == 31)
-                        result = "1 Февраль (01.02.2025)";
+                        result = "01.02";
                     else
-                        result = $"{n + 1} Январь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.01.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.01";
                     break;
                 case 2:
                     if (n == 28)
-                        result = "1 Март (01.03.2025)";
+                        result = "01.03";
                     else
-                        result = $"{n + 1} Февраль ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.02.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.02)";
                     break;
                 case 3:
                     if (n == 31)
-                        result = "1 Апрель (01.04.2025)";
+                        result = "01.04";
                     else
-                        result = $"{n + 1} Март ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.03.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.03)";
                     break;
                 case 4:
                     if (n == 30)
-                        result = "1 Май (01.05.2025)";
+                        result = "01.05";
                     else
-                        result = $"{n + 1} Апрель ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.04.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.04)";
                     break;
                 case 5:
                     if (n == 31)
-                        result = "1 Июнь (01.06.2025)";
+                        result = "01.06";
                     else
-                        result = $"{n + 1} Май ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.05.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.05)";
                     break;
                 case 6:
                     if (n == 30)
-                        result = "1 Июль (01.07.2025)";
+                        result = "01.07";
                     else
-                        result = $"{n + 1} Июнь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.06.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.06";
                     break;
                 case 7:
                     if (n == 31)
-                        result = "1 Август (01.08.2025)";
+                        result = "01.08";
                     else
-                        result = $"{n + 1} Июль ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.07.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.07";
                     break;
                 case 8:
                     if (n == 31)
-                        result = "1 Сентябрь (01.09.2025)";
+                        result = "01.09";
                     else
-                        result = $"{n + 1} Август ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.08.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.08";
                     break;
                 case 9:
                     if (n == 30)
-                        result = "1 Октябрь (01.10.2025)";
+                        result = "01.10";
                     else
-                        result = $"{n + 1} Сентябрь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.09.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.09";
                     break;
                 case 10:
                     if (n == 31)
-                        result = "1 Ноябрь (01.11.2025)";
+                        result = "01.11";
                     else
-                        result = $"{n + 1} Октябрь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.10.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.10";
                     break;
                 case 11:
                     if (n == 30)
-                        result = "1 Декабрь (01.12.2025)";
+                        result = "01.12";
                     else
-                        result = $"{n + 1} Ноябрь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.11.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.11";
                     break;
                 case 12:
                     if (n == 31)
-                        result = "1 Январь (01.01.2026)";
+                        result = "01.01";
                     else
-                        result = $"{n + 1} Декабрь ({(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.12.2025)";
+                        result = $"{(n + 1 > 9 ? n + 1 : "0" + (n + 1))}.12";
                     break;
                 default:
                     throw new ArgumentException("Номер месяца должен быть от 1 до 12!");
